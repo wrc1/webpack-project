d78bcccd6603fa89bc6cc8b5cb7626439f5f8d67
wrc1 <wrcron1@gmail.com>
2019-02-01 17:58:40 +0200

    add react start page react to project

--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
 require("babel-runtime/regenerator")
+require("babel-register");
 require("webpack-hot-middleware/client?reload=true")
 require("./main.css")
 require("./images/link.jpg")
 require("./index.html")
+require("./app/index.js")
 
-var a = async args => {
-  const { a, b } = args
-  await console.log("Hello from the future!", a, b)
-}
-
-a({ a: 1, b: 2 })
