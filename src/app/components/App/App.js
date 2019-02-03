e214e4cd434e32b354fdae700a92f8ebe778e8c1
wrc1 <wrcron1@gmail.com>
2019-02-03 13:49:44 +0200

    add components folder, add env file, change files stracture

---
+++ src/app/components/App/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div class="profile">
+          <img src="./images/link.jpg" alt="" />
+          <h1>Hello </h1>
+        </div>
+      </div> 
+    );
+  }
+}
+
+export default App;
--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 require("babel-runtime/regenerator")
 require("babel-register");
 require("webpack-hot-middleware/client?reload=true")
-require("./main.css")
-require("./images/link.jpg")
+// require("./main.css")
+// require("./images/link.jpg")
 require("./index.html")
 require("./app/index.js")
 
